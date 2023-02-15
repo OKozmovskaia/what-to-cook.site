5a438df7cd1549cc4832d0d70d8d5bbca1bdeb5b
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-02-15 19:57:58 +0100

    add sticky effect on scroll to navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import useStickyOnScroll from "../../hooks/useStickyOnScroll";
 import styles from "./navbar.module.css";
+import cn from "classnames";
 
 export default function Navbar() {
+  const { sticky, stickyRef } = useStickyOnScroll();
+
   return (
-    <div className={styles.container}>
+    <div
+      className={cn(styles.container, { [styles.sticky]: sticky })}
+      ref={stickyRef}
+    >
       <p>Home</p>
       <p>My recipes</p>
       <p>My product</p>
