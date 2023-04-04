e6287c0e3f4a52c919dbc8cc3bb62ec148bf0b7e
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-04-04 15:27:44 +0200

    update Footer component

--- client_side/src/components/Footer/footer.js
+++ client_side/src/components/Footer/footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../Button";
+import { Link } from "react-router-dom";
 
 import styles from "./footer.module.css";
 
@@ -7,10 +8,17 @@ const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
       <div className={styles.iconContainer}>
-        <Button iconStyle icon="cv" />
-        <Button iconStyle icon="email" />
-        <Button iconStyle icon="github" />
-        <Button iconStyle icon="linkedin" />
+        <Link to="mailto:info@what-to-cook.syte">
+          <Button iconStyle icon="email" />
+        </Link>
+
+        <Link to="https://github.com/OKozmovskaia/what-to-cook.site">
+          <Button iconStyle icon="github" />
+        </Link>
+
+        <Link to="https://www.linkedin.com/in/olga-kozmovskaia/">
+          <Button iconStyle icon="linkedin" />
+        </Link>
       </div>
       <p>© Olga Kozmovskaia</p>
     </footer>
